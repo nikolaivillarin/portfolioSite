315f265d71d54a29aba7f6228b9adb9435c507a8
nikol3x <nikolai.villarin@gmail.com>
2017-03-27 11:01:32 -0400

    Updated script references for minification

--- Nikolai/App_Start/BundleConfig.cs
+++ Nikolai/App_Start/BundleConfig.cs
@@ -39,6 +39,38 @@ namespace Nikolai
                 , "~/Scripts/floatingBall.js"
                 , "~/ViewScripts/loader.js" 
             ));
+
+            bundles.Add(new ScriptBundle("~/Content/homePage").Include(
+                "~/ViewScripts/homePage.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/contactPage").Include(
+                "~/ViewScripts/contactPage.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/menuPage").Include(
+                "~/ViewScripts/menuPage.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/workPage").Include(
+                "~/ViewScripts/workPage.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/workBoxgrove").Include(
+                "~/ViewScripts/workBoxgrove.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/workHR").Include(
+                "~/ViewScripts/workHR.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/workMultiplex").Include(
+                "~/ViewScripts/workMultiplex.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/workRateQuote").Include(
+                "~/ViewScripts/workRateQuote.js"
+            ));
         }
     }
 }
